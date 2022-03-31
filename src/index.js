bd35b1eb12e3386ec1ee38da4e5fbc88ed8a8282
=Higo Chu <=higochu@motivesvn.com>
2022-03-31 16:18:18 +0700

    first commit

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const App =()=>{
+    return (
+        <div>
+            <h1>Xin chao, mung ban den voi webpack</h1>
+            <h2>hello, i can do it</h2>
+        </div>
+    )
+}
+ReactDOM.render(<App/> , document.getElementById("root"));
